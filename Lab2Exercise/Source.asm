73944989181e74c4f9162dab8da3e2765094507b
dhmhgre <dhmhgre@gmail.com>
2020-03-27 10:51:41 +0200

    Implemented Askisi1

--- Lab2Exercise/Source.asm
+++ Lab2Exercise/Source.asm
@@ -21,7 +21,41 @@ z SWORD -30d
 
 .code
 main PROC
-	
+	; *** ASKISI 1 ***
+	mov edx, OFFSET mCalculating	; Load EDX with the mCalculating string OFFSET
+	call WriteString				; Show the message on screen
+
+	; Calculating address of array1(2,2).
+	; The result will be 32bit hence the arithmetic operations 
+	; are performed on 32bit registers
+	movzx ebx, x					; move the row number into ebx register 
+	shl   ebx, 2					; EBX = row * 4 
+	movzx ecx, y					; move the row number into ecx register 
+	add   ebx,ecx					; EBX = row * 4 + column
+	shl   ebx,2						; EBX = (row * 4 + column) * 4
+
+	mov eax,[array1+ebx]			; mov designated array element to ECX using INDIRECT ADDRESSING
+	mov edx, OFFSET mAccessElement  ; Load EDX with the mAccessElement string OFFSET
+	call WriteString				; Show the message on screen
+	call WriteInt					; Show the element value on screen
+	call Crlf						; Go to next line
+
+	movsx ecx,z						; move the value of z into ECX
+	add	  eax,ecx					; EAX = 22 + ECX = -8
+	mov edx, OFFSET mResultSum		; Load EDX with the mResultSum string OFFSET
+	call WriteString				; Show the message on screen
+	call WriteInt					; Show the element value on screen
+	call Crlf						; Go to next line
+
+	mov eax,[array1+ebx]			; move the value of z into EBX
+	sub	 eax,ECX					; EAX = 22 - ECX = 52
+	mov edx, OFFSET mResultDiff		; Load EDX with the mResultSum string OFFSET
+	call WriteString				; Show the message on screen
+	call WriteInt					; Show the element value on screen
+	call Crlf						; Go to next line
+
+	; *** ASKISI 2 ***
+
 
 	exit
 main ENDP
